4b40d46745a509e50cbfb7f69b148d0419b11725
Benedict Notoane <benni@MacBookPro.lan>
2025-04-22 21:35:26 +0200

    created a reusable loading spinner component

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,19 @@ import styles from "./App.module.css";
 import Button from "./components/Button/Button";
 import Counter from "./components/Counter/Counter";
 import Input from "./components/Input/Input";
+import Spinner from "./components/Spinner/Spinner";
 
 const App = () => {
   return (
     <div className={styles.rootContainer}>
       {/* <Button /> */}
       {/* <Counter /> */}
-      <Input
+      {/* <Input
         placeholder="Enter first name e.g. John"
         label="First name"
         errorMessage="First name is required"
-      />
+      /> */}
+      <Spinner />
     </div>
   );
 };
