aa781b52faea02874690ad8ff54802ad2f4896d7
Benedict Notoane <benni@MacBookPro.lan>
2025-04-22 20:57:09 +0200

    created reusable button component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from "./App.module.css";
 import Button from "./components/Button/Button";
+import Counter from "./components/Counter/Counter";
 
 const App = () => {
   return (
     <div className={styles.rootContainer}>
-      <Button />
+      <Counter />
+      {/* <Button>Add</Button> */}
     </div>
   );
 };
